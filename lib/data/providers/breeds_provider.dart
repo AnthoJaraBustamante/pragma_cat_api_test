5e365e8337812226b506001058af8a136a1106f0
Anthony Josué Jara Bustamante <omarbush81@gmail.com>
2025-03-06 03:44:19 -0500

    Agregar detalle de raza por ID

--- lib/data/providers/breeds_provider.dart
+++ lib/data/providers/breeds_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pragma_cat_api_test/data/models/breed.dart';
 import 'package:pragma_cat_api_test/data/repositories/breeds_repository.dart';
 import 'package:pragma_cat_api_test/data/services/service_locator.dart';
@@ -12,4 +13,10 @@ class BreedsState extends _$BreedsState {
   FutureOr<List<Breed>> build() async {
     return await breedsRepository.fetchBreeds();
   }
+}
+
+@riverpod
+Breed? breedDetail(Ref ref, String id) {
+  final listBreeds = ref.watch(breedsStateProvider).valueOrNull;
+  return listBreeds?.firstWhere((breed) => breed.id == id);
 }
--- lib/data/providers/breeds_provider.g.dart
+++ lib/data/providers/breeds_provider.g.dart
@@ -6,6 +6,157 @@ part of 'breeds_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+String _$breedDetailHash() => r'46065f81681ce1b803425a64e29b2b186014847e';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+/// See also [breedDetail].
+@ProviderFor(breedDetail)
+const breedDetailProvider = BreedDetailFamily();
+
+/// See also [breedDetail].
+class BreedDetailFamily extends Family<Breed?> {
+  /// See also [breedDetail].
+  const BreedDetailFamily();
+
+  /// See also [breedDetail].
+  BreedDetailProvider call(
+    String id,
+  ) {
+    return BreedDetailProvider(
+      id,
+    );
+  }
+
+  @override
+  BreedDetailProvider getProviderOverride(
+    covariant BreedDetailProvider provider,
+  ) {
+    return call(
+      provider.id,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'breedDetailProvider';
+}
+
+/// See also [breedDetail].
+class BreedDetailProvider extends AutoDisposeProvider<Breed?> {
+  /// See also [breedDetail].
+  BreedDetailProvider(
+    String id,
+  ) : this._internal(
+          (ref) => breedDetail(
+            ref as BreedDetailRef,
+            id,
+          ),
+          from: breedDetailProvider,
+          name: r'breedDetailProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$breedDetailHash,
+          dependencies: BreedDetailFamily._dependencies,
+          allTransitiveDependencies:
+              BreedDetailFamily._allTransitiveDependencies,
+          id: id,
+        );
+
+  BreedDetailProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.id,
+  }) : super.internal();
+
+  final String id;
+
+  @override
+  Override overrideWith(
+    Breed? Function(BreedDetailRef provider) create,
+  ) {
+    return ProviderOverride(
+      origin: this,
+      override: BreedDetailProvider._internal(
+        (ref) => create(ref as BreedDetailRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        id: id,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeProviderElement<Breed?> createElement() {
+    return _BreedDetailProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is BreedDetailProvider && other.id == id;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, id.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin BreedDetailRef on AutoDisposeProviderRef<Breed?> {
+  /// The parameter `id` of this provider.
+  String get id;
+}
+
+class _BreedDetailProviderElement extends AutoDisposeProviderElement<Breed?>
+    with BreedDetailRef {
+  _BreedDetailProviderElement(super.provider);
+
+  @override
+  String get id => (origin as BreedDetailProvider).id;
+}
+
 String _$breedsStateHash() => r'f46e44e804186841557264fc5a50d2ea39475372';
 
 /// See also [BreedsState].
